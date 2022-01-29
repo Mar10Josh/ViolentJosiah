5eb407e169640a2f115b99f12f5a43b513550674
Mar10Josh <65928867+Mar10Josh@users.noreply.github.com>
2022-01-28 17:14:49 -1000

    Update josiah_docubuilder.user.js

--- josiah_docubuilder.user.js
+++ josiah_docubuilder.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name        Docubuilder
 // @namespace   Editors
-// @match       *
+// @match       *://*/*
 // @grant       none
-// @version     1.0
+// @version     1.1
 // @author      Josiah
 // @description Josiah's DocuBuilder builds anything on the current document. This also works on HTML files!
 // ==/UserScript==
